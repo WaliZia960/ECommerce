56cead87c1ba3ba27961ac4f38e6ef04814d934b
MuhammadW-a <67828032+MuhammadW-a@users.noreply.github.com>
2021-02-03 07:45:13 -0800

    EncodeCopy Method Added

--- Controllers/Encode.cs
+++ Controllers/Encode.cs
@@ -22,6 +22,21 @@ namespace ECommerce.Controllers
             }
         }
 
+        public static string EncodePasswordToBase64Copy(string password)
+        {
+            try
+            {
+                byte[] encData_byte = new byte[password.Length];
+                encData_byte = System.Text.Encoding.UTF8.GetBytes(password);
+                string encodedData = Convert.ToBase64String(encData_byte);
+                return encodedData;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Error in base64Encode" + ex.Message);
+            }
+        }
+
 
         // to decode password
         /*
